b2664d4aa3e818904a445bd888f223feaf646d37
Ndunge-Makau <105147786+Ndunge-Makau@users.noreply.github.com>
2022-09-20 15:29:36 +0300

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-for x in range(0, 10):
-    for y in range(x + 1, 10):
-        if x == 8 and y == 9:
-            print("{}{}".format(x, y))
-        else:
-            print("{}{}".format(x, y), end=", ")
+for i in range(0, 100):
+    ones = i % 10
+    tens = i // 10
+
+    if tens == ones:
+        continue
+    while tens < ones:
+        print(f"{i:02}")
+        i += 1
+        break
