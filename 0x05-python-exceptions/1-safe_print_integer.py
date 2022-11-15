834f6cca99d460f89c343d64fb0f148203dc0a73
Ndunge-Makau <winniemakau@outlook.com>
2022-11-15 05:44:34 -0800

    Update 1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-def safe_print_integer(value):
 
+def safe_print_integer(value):
     try:
-        print('{:d}'.format(value))
-    except Exception as error:
-        return False
-    else:
+        print("{:d}".format(value))
         return True
+    except ValueError:
+        return False
