f78c63f4515780e7c8b865a47cf430af1ddd4206
Ndunge-Makau <winniemakau@outlook.com>
2023-11-02 03:09:16 -0700

    Fix 5-text_indentation.py.

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -8,8 +8,7 @@ Defines the text_indentation module
 def text_indentation(text):
     """
      prints a text with 2 new lines after each of these characters
-     ., ? and :
-     """
+    """
     if type(text) is not str:
         raise TypeError("text must be a string")
 
