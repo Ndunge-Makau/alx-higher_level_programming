5a1828b058aa6aff056de3e5091d8dc3ccd161b9
Ndunge-Makau <winniemakau@outlook.com>
2023-11-08 00:45:04 -0800

    Add 1-my_list.py.

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Defines the MyList class.
+
+"""
+
+
+class MyList(list):
+    """
+    Class MyList that inherits from list.
+    """
+    
+    def print_sorted(self):
+        """Prints sorted list"""
+        return print((sorted(self)))
