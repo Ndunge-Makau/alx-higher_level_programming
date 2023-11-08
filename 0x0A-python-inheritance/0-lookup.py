2afd3897a54cea58ae99d3bc086708100ec06e3d
Ndunge-Makau <winniemakau@outlook.com>
2023-11-08 00:15:35 -0800

    Add 0-lookup.py.

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+The lookup module
+"""
+
+
+def lookup(obj):
+    """
+    Returns the list of available attributes and methods of an object.
+    """
+    return list(obj.__dict__)
