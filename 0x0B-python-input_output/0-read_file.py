dfb8a04ed32d0bb29db7d57eeeae02c01baaf357
Ndunge-Makau <winniemakau@outlook.com>
2023-11-17 01:23:11 -0800

    Add 0-read_file.py.

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+The 0-read_file.py module: read_file() function.
+
+"""
+
+
+def read_file(filename=""):
+    """Reads a text file (UTF8) and prints it to stdout."""
+    with open(filename, encoding="utf-8") as my_file:
+        my_file.read()
