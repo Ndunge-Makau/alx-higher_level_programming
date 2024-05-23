d3516957c8090a8b32e9f3eabaedd027b212477b
Ndunge-Makau <winniemakau@outlook.com>
2024-05-23 02:37:25 -0700

    Fix 100-move_to_utf8.sql: Collate option.

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,5 @@
 -- Converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci) in your MySQL server.
 USE hbtn_0c_0;
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table CHARACTER SET utf8mb4;
 ALTER TABLE first_table MODIFY name varchar(256) CHARACTER SET utf8mb4;
