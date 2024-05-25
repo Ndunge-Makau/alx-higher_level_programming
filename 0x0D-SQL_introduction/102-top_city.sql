157188e20baf0edb6c81c693015be6f6ac7b71a6
Ndunge-Makau <winniemakau@outlook.com>
2024-05-25 06:53:37 -0700

    Fix display column.

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,7 @@
 -- Displays the top 3 of cities temperature during July and August ordered by temperature (descending)
-SELECT city, AVG(value) AS avg_value
+SELECT city, AVG(value) AS avg_temp
 FROM temperatures
 WHERE temperatures.month = 7 OR temperatures.month = 8
 GROUP BY city
-ORDER BY avg_value DESC
+ORDER BY avg_temp DESC
 LIMIT 3;
