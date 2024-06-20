bea1f1424b24e8dfe83b2e5b979449b38313c79f
Ndunge-Makau <winniemakau@outlook.com>
2024-06-19 23:42:11 -0700

    Add 11-second_biggest.js.

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+let end = process.argv.length - 1;
+let myArray = process.argv.slice(2, end);
+
+myArray.sort().reverse();
+if (myArray.length == 0 || myArray.length == 1) {
+  console.log(0);
+} else {
+  console.log(myArray[1]);
+}
