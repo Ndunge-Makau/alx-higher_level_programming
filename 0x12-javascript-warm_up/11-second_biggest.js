361089923d44dfe41868a3ed9cd9e2afc1624019
Ndunge-Makau <winniemakau@outlook.com>
2024-06-20 00:11:24 -0700

    Fix 11-second_biggest.js: Fix sort() and make Semistandard compliant.

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-let end = process.argv.length - 1;
-let myArray = process.argv.slice(2, end);
+const end = process.argv.length;
+const myArray = process.argv.slice(2, end);
 
 myArray.sort((a, b) => a - b).reverse();
-if (myArray.length == 0 || myArray.length == 1) {
+if (myArray.length === 0 || myArray.length === 1) {
   console.log(0);
 } else {
   console.log(myArray[1]);
