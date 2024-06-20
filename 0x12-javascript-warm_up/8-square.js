7e154e5487e5215e53a3a77f7071d3914d75bea9
Ndunge-Makau <winniemakau@outlook.com>
2024-06-19 22:47:37 -0700

    Fix 8-square.js: Make file Semistandard compliant.

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,13 +4,13 @@ const arg = process.argv[2];
 const number = parseInt(arg);
 
 if (Number.isInteger(number)) {
-  for (i = 0; i < number; i++) {
+  for (let i = 0; i < number; i++) {
     let myXs = '';
-    for (j = 0; j < number; j++) {
+    for (let j = 0; j < number; j++) {
       myXs += 'X';
     }
     console.log(myXs);
   }
 } else {
-  console.log('Missing Size');
+  console.log('Missing size');
 }
