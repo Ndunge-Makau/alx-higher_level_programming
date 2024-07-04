8fbde765c5cf5e51960d93ea01af9ae2d114339e
Ndunge-Makau <winniemakau@outlook.com>
2024-07-04 04:53:50 -0700

    Fix 14-model_city_fetch_by_state.py.

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -10,11 +10,11 @@ from model_state import State, Base
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], '',
+                           .format(sys.argv[1], sys.argv[2],
                                    sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     session = Session(bind=engine)
-    my_query = session.query(City.name, City.id, State.name).
-    join(State, City.state_id == State.id).all()
+    my_query = session.query(City.name, City.id, State.name).\
+        join(State, City.state_id == State.id).all()
     for city in my_query:
         print("{}: ({}) {}".format(city[2], city[1], city[0]))
