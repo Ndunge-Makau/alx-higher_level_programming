d99ad2e7cf15caaa421b49d6bbdfb597e6968789
Ndunge-Makau <winniemakau@outloo.com>
2024-10-16 18:17:03 +0300

    Fix 3-methods.sh: Fix curl arguments.

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL and displays all HTTP methods the server will accept.
-curl -sLI 0.0.0.0:5000/route_4 | grep Allow: | awk '{for (i=2; i<= NF; i++) {printf $i; if (i < NF) printf " ";} printf "\n"}'
+ curl -sLI $1 | grep Allow: | awk '{for (i=2; i<= NF; i++) {printf $i; if (i < NF) printf " ";} printf "\n"}'
