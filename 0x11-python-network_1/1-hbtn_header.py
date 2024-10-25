cbf2ef57abe1663cd9a2faf52397431a5343a854
Ndunge-Makau <winniemakau@outloo.com>
2024-10-25 12:13:08 +0300

    Fix 1-hbtn_header.py: Try documentation fix.

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Displays the value of the X-Request-Id found in the header of the response.
+"""Displays the X-Request-Id header variable of a request to a given URL.
 Usage: ./1-hbtn_header.py <URL>
 """
 
