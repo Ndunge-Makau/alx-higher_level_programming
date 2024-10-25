57266a459e59ae537ab2b5a9efa7d74e65ed1306
Ndunge-Makau <winniemakau@outloo.com>
2024-10-25 17:06:18 +0300

    Fix 2-post_email.py.

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,29 +1,18 @@
 #!/usr/bin/python3
-"""Sends a POST request to the passed URL with the email as a parameter,
-    and displays the body of the response (decoded in utf-8)
-    Usage: ./2-post_email.py <URL> <EMAIL>
+"""Sends a POST request to a given URL with a given email.
+Usage: ./2-post_email.py <URL> <email>
+  - Displays the body of the response.
 """
-
 import sys
 import urllib.parse
 import urllib.request
-import urllib.error
 
-if __name__ == '__main__':
 
+if __name__ == "__main__":
     url = sys.argv[1]
-    values = {"email": sys.argv[2]}
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
 
-    data = urllib.parse.urlencode(values).encode("ascii")
     request = urllib.request.Request(url, data)
-
-    try:
-        with urllib.request.urlopen(request) as response:
-            print(response.read().decode('utf-8'))
-    except urllib.error.URLError as e:
-        url = sys.argv[1] + '/post_email'
-        values = {"email": sys.argv[2]}
-        data = urllib.parse.urlencode(values).encode("ascii")
-        request = urllib.request.Request(url, data)
-        with urllib.request.urlopen(request) as response:
-            print(response.read().decode('utf-8'))
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
