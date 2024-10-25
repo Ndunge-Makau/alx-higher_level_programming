72d9baad702e94d725bc4f4d92476ffcbbea33f8
Ndunge-Makau <winniemakau@outloo.com>
2024-10-25 12:24:57 +0300

    Fix 1-hbtn_header.py: Fix documentation and make pycodestyle compliant.

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """Displays the X-Request-Id header variable of a request to a given URL.
-Usage: ./1-hbtn_header.py <URL> 
+Usage: ./1-hbtn_header.py <URL>
 """
+
 import sys
 import urllib.request
 
 if __name__ == "__main__":
-	url = sys.argv[1]
-	
-	with urllib.request.urlopen(url) as response:
-		print(response.headers.get('X-Request-Id'))
-
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        print(response.headers.get('X-Request-Id'))
