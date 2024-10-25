fa56f72246d69e694982400378811f4a15c6476f
Ndunge-Makau <winniemakau@outloo.com>
2024-10-25 12:17:30 +0300

    Fix 1-hbtn_header.py: Try documentation fix.

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,8 +3,8 @@
 import sys
 import urllib.request
 
-if __name__ == "__main__":
-    url = sys.argv[1]
-    
-    with urllib.request.urlopen(url) as response:
-        print(response.headers.get('X-Request-Id'))
+"""if __name__ == "__main__":"""
+url = sys.argv[1]
+
+with urllib.request.urlopen(url) as response:
+	print(response.headers.get('X-Request-Id'))
