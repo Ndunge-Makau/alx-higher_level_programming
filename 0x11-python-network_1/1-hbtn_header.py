3cd0f9e0a94ef62735027a030b7d7d5c52b432fe
Ndunge-Makau <winniemakau@outloo.com>
2024-10-25 12:06:59 +0300

    Fix 1-hbtn_header.py: Try fix.

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,31 +1,14 @@
 #!/usr/bin/python3
+"""Displays the X-Request-Id header variable of a request to a given URL.
+Usage: ./1-hbtn_header.py <URL>
 """
-Sends an HTTP request to the provided URL and displays the value of the 'X-Request-Id' 
-header from the server's response.
-
-Usage:
-    ./script.py <URL>
-
-Parameters:
-    <URL>: The URL to which the HTTP request is sent.
-
-Output:
-    Prints the value of the 'X-Request-Id' header from the HTTP response.
-
-Example:
-    $ ./script.py http://example.com
-    12345-abcdef
-
-Raises:
-    urllib.error.URLError: If there is an issue with the network or the URL is invalid.
-    urllib.error.HTTPError: If the server returns an HTTP error code.
-"""
-
-
 import sys
 import urllib.request
 
-url = sys.argv[1]
 
-with urllib.request.urlopen(url) as response:
-    print(response.headers.get('X-Request-Id'))
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
