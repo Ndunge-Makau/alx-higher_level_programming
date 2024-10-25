3a5074195ffe55db858cc4410688eb05c1f04c5a
Ndunge-Makau <winniemakau@outloo.com>
2024-10-25 11:43:21 +0300

    Fix 1-hbtn_header.py: Requirement to use get.

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL, sends a request to the URL and
-displays the value of the X-Request-Id variable found
-in the header of the response."""
+"""Sends a request to the URL and displays the value of the X-Request-Id"""
 
 import sys
 import urllib.request
@@ -10,4 +8,4 @@ import urllib.request
 url = sys.argv[1]
 
 with urllib.request.urlopen(url) as response:
-    print(response.headers['X-Request-Id'])
+    print(response.headers.get('X-Request-Id'))
