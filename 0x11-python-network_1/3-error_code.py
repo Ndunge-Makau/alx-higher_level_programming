107d5d7ea48c66f22056b5acfa2864aed603813d
Ndunge-Makau <winniemakau@outloo.com>
2024-10-28 17:20:07 +0300

    Fix 3-error_code.py: Make sure output is decoded.

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,10 +8,10 @@ import urllib.request
 import urllib.error
 
 if __name__ == '__main__':
-    url = urllib.request.Request(sys.argv[1])
+    url = sys.argv[1]
 
     try:
         with urllib.request.urlopen(url) as response:
-            print(response.read())
+            print(response.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
         print("Error Code: {}".format(e.code))
